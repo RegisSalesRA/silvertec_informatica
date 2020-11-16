91b5bc391d1c7e3c677500a924bb2b2abdf9061f
Regis Sales <regis@prodadospesquisa.com.br>
2020-11-16 15:15:56 -0300

    Refactory classes and admin

--- produtos/admin.py
+++ produtos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Processador, PlacaMae, PlacaDeVideo, MemoriaRam
 
 # Register your models here.
+
+admin.site.register(Processador)
+admin.site.register(PlacaMae)
+admin.site.register(MemoriaRam)
+admin.site.register(PlacaDeVideo)
--- produtos/models.py
+++ produtos/models.py
@@ -1,18 +1,48 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Processador(models.Model):
-    nome = models.CharField(max_length=150,)
+    nome = models.CharField(max_length=150, )
 
+    class Meta:
+        verbose_name = 'Processador'
+        verbose_name_plural = "Processadores"
+
+    def __str__(self):
+        return self.nome
 
 class PlacaMae(models.Model):
-    nome = models.CharField(max_length=150,)
+    nome = models.CharField(max_length=150, )
 
+
+    class Meta:
+        verbose_name = 'Placa Mãe'
+        verbose_name_plural = "Placas Mãe"
+
+    def __str__(self):
+        return self.nome
 
 class MemoriaRam(models.Model):
-    nome = models.CharField(max_length=150,)
+    nome = models.CharField(max_length=150, )
 
+
+    class Meta:
+        verbose_name = 'Memoria Ram'
+        verbose_name_plural = "Memorias"
+
+    def __str__(self):
+        return self.nome
 
 class PlacaDeVideo(models.Model):
-    nome = models.CharField(max_length=150,)
+    nome = models.CharField(max_length=150, )
+
+
+    class Meta:
+        verbose_name = 'Placa de video'
+        verbose_name_plural = "Placas de video"
+
+
+    def __str__(self):
+        return self.nome
